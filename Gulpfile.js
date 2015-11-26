625c071de90d2ad3101b42565e8165f51ec589a1
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2015-11-26 22:01:28 +0100

    Gulp Refactor
    a little gulp file refactor so the client is placed inside the server

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,11 @@ var gulp 	 = require('gulp'),
 	del = require('del'),
 	sourcemaps = require('gulp-sourcemaps'),
 	es = require('event-stream'),
-	changed = require('gulp-changed');
+	changed = require('gulp-changed'),
+	runSequence = require('run-sequence');
 
-var dist = 'dist/';
+var distServer = 'dist/';
+var distClient = 'dist/client/';
 var temp = 'dist/temp/';
 
 gulp.task('clean', function() {
@@ -14,11 +16,11 @@ gulp.task('clean', function() {
 
 gulp.task('copyBuildStage', function(){
 	return es.merge(gulp.src(['src/**/*.*', '!src/shared/**/*.js'])
-						.pipe(changed(dist))
+						.pipe(changed(distServer))
 						.pipe(gulp.dest(temp)),
 
 					gulp.src('src/shared/**/*.*')
-						.pipe(changed(dist + 'frontend/'))
+						.pipe(changed(distServer))
 						.pipe(gulp.dest(temp + 'frontend/'))
 						.pipe(gulp.dest(temp + 'backend/'))
 				);
@@ -26,7 +28,7 @@ gulp.task('copyBuildStage', function(){
 
 gulp.task('compile', ['copyBuildStage'], function(){
 	return gulp.src([temp + '/**/*.js', '!**/node_modules/**', '!**/frontend/libs/**'])
-		.pipe(changed(dist))
+		.pipe(changed(distServer))
 		.pipe(sourcemaps.init())
 			.pipe(babel({
 				presets : ['es2015'],
@@ -37,8 +39,12 @@ gulp.task('compile', ['copyBuildStage'], function(){
 });
 
 gulp.task('copyToDist', ['copyBuildStage', 'compile'],  function(){
-	return gulp.src(temp + '**')
-		.pipe(gulp.dest(dist));
+	return es.merge(gulp.src(temp + 'backend/**')
+						.pipe(gulp.dest(distServer)),
+
+					gulp.src(temp + 'frontend/**')
+						.pipe(gulp.dest(distClient))
+				);
 });
 
 gulp.task('clean:temp', ['copyToDist'], function(){
@@ -48,21 +54,23 @@ gulp.task('clean:temp', ['copyToDist'], function(){
 gulp.task('copyDependecies', function(){
 	return es.merge(
 		gulp.src('node_modules/systemjs/dist/system.js')
-			.pipe(gulp.dest(dist + 'frontend/libs/')),
+			.pipe(gulp.dest(distClient + 'libs/')),
 
 		gulp.src(['bower_components/angular-material/angular-material.min.css'])
-			.pipe(gulp.dest(dist + 'frontend/stylesheets/angular')),
+			.pipe(gulp.dest(distClient + 'stylesheets/angular')),
 
 		gulp.src(['bower_components/angular/angular.min.js',
 				  'bower_components/angular-aria/angular-aria.min.js',
 				  'bower_components/angular-animate/angular-animate.min.js',
 				  'bower_components/angular-material/angular-material.min.js'])
-			.pipe(gulp.dest(dist + 'frontend/libs/angular/'))
+			.pipe(gulp.dest(distClient + 'libs/angular/'))
 	);
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', ['default'], function(){
 	gulp.watch(['src/**/*.*'], ['copyToDist', 'clean:temp']);
 })
 
-gulp.task('default', ['compile', 'copyDependecies', 'copyToDist', 'clean:temp']);
+gulp.task('default', function(cb){
+	runSequence('clean', ['copyDependecies', 'clean:temp'], cb);
+});
