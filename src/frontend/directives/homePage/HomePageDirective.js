1b5ece553c7f644be8173396aed2944b33c898e8
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2015-12-16 20:46:34 +0100

    Home Page
    - reset header when leaving
    - remove carousel margin

--- src/frontend/directives/homePage/HomePageDirective.js
+++ src/frontend/directives/homePage/HomePageDirective.js
@@ -1,5 +1,6 @@
-require('./slide/SlideDirective.js');
-require('./HomePageController.js');
+import './slide/SlideDirective.js';
+import './HomePageController.js';
+import UiService from 'services/UiService.js';
 
 
 angular.module('app-mmi').directive('appPageHome', function(){
@@ -7,6 +8,11 @@ angular.module('app-mmi').directive('appPageHome', function(){
 		restrict : 'E',
 		scope : {},
 		templateUrl : './directives/homePage/Template.html',
-        controller : 'HomePageController'
+        controller : 'HomePageController',
+        link : function(scope, element) {
+            element.on('$destroy', () => {
+                UiService.header.status = 'raised';
+            })
+        }
 	}
 })
