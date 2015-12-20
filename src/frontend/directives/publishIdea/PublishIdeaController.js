81326f53c0235794f8f120d9db6c8126a2a09886
Philipp Beck <dertieran@gmail.com>
2015-12-20 13:59:43 +0100

    Worked on publishIdea
    - you can now create and save valid Projects to the Storage
    - jobList is still in the frontend

--- src/frontend/directives/publishIdea/PublishIdeaController.js
+++ src/frontend/directives/publishIdea/PublishIdeaController.js
@@ -6,6 +6,12 @@ let logger = Make(Logger)('PublishIdeaController');
 
 angular.module('app-mmi').controller("PublishIdeaController", ['$scope', function($scope) {
 
+    $scope.jobList = ['Web Developer', 'Java Developer', 'C# Developer', 'Web Designer', 'Storywriter', 'Character Designer', 'Front End Developer'];
+
+    $scope.project = {};
+    $scope.project.members = [{}];
+    $scope.project.categories = [];
+
     $scope.submit = function(){
         logger.log($scope.project);
         PublishIdeaService.publish($scope.project)
@@ -13,5 +19,16 @@ angular.module('app-mmi').controller("PublishIdeaController", ['$scope', functio
 
     $scope.reset = function() {
         $scope.project = {}
+        $scope.project.members = [{}];        
+        $scope.project.categories = [];
+    };
+
+    $scope.addNewChoice = function() {
+        $scope.project.members.push({});
+    };
+
+    $scope.removeChoice = function() {
+        var lastItem = $scope.project.members.length-1;
+        $scope.project.members.splice(lastItem);
     };
 }])
